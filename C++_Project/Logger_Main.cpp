2b9c740714f37b39a2faeb23537250a44620f446
ltw <3245849061@qq.com>
2021-01-13 21:21:31 +0800

    完善日志等级+用户可以设定输出日志类型

--- C++_Project/Logger_Main.cpp
+++ C++_Project/Logger_Main.cpp
@@ -17,11 +17,16 @@ void func1() {
 }
 
 void func2() {
-	LOG_TRACE << "1" << "2" << "3" << "4" << "5" << "6";
+	LOG_INFO << "info 1" << "2" << "3" << "4" << "5" << "6";
+	LOG_DEBUG << "debug 1" << "2" << "3" << "4" << "5" << "6";
+	LOG_WARN << "warn 1" << "2" << "3" << "4" << "5" << "6";
+	LOG_ERROR << "error 1" << "2" << "3" << "4" << "5" << "6";
 	return ;
 }
 
 int main() {
+	//设置输出日志等级，当前只允许info类型的输出 
+	LOG_SET(haizei::LogLevel::INFO); 
 	haizei::thread_pool tp(10);
 	tp.start();
 	for (int i = 1; i <= 100; i++) {
